d4ecd3bf54698d382328cf730e3a68fce05775fa
Despaux <guillaume.despaux@algosup.com>
2024-02-08 16:30:36 +0100

    Test file, commentary + organization

--- test/test.cc
+++ test/test.cc
@@ -12,136 +12,66 @@
 #include "builder.h"
 
 /*
- *  parseArgs
- */
-
-// Test help flag
-TEST(parseArgs, help) {
-  char *argv[] = {"test", "-h"};
-  int argc = sizeof(argv) / sizeof(argv[0]);
-  flags_t flags = parseArgs(argc, argv);
-  ASSERT_TRUE(flags.help);
-}
-// Test version flag
-TEST(parseArgs, version) {
-  char *argv[] = {"test", "-v"};
-  int argc = sizeof(argv) / sizeof(argv[0]);
-  flags_t flags = parseArgs(argc, argv);
-  ASSERT_TRUE(flags.version);
-}
-// Test verbose flag
-TEST(parseArgs, verbose) {
-  char *argv[] = {"test", "-V"};
-  int argc = sizeof(argv) / sizeof(argv[0]);
-  flags_t flags = parseArgs(argc, argv);
-  ASSERT_TRUE(flags.verbose);
-}
-// Test debug flag
-TEST(parseArgs, debug) {
-  char *argv[] = {"test", "-d"};
-  int argc = sizeof(argv) / sizeof(argv[0]);
-  flags_t flags = parseArgs(argc, argv);
-  ASSERT_TRUE(flags.debug);
-}
-// Test false argv
-TEST(parseArgs, invalidFlag) {
-  char *argv[] = {"iat2.exe", "-f"};
-  int argc = sizeof(argv) / sizeof(argv[0]);
-  flags_t flags = parseArgs(argc, argv);
-  ASSERT_FALSE(flags.help);
-}
-
-/*
  *  checkAOPFile
  */
 
-// this test won't display a message because the test just above already deals with the same function.
 TEST(checkAOPFile, invalidFileName) {
-  char* invalidFileName = "test.txt"; // Replace fileName with an invalid file name
-  checkAOPFile(invalidFileName);
-  ASSERT_TRUE(EXIT_FAILURE);
-}
-/*
- *  isInt
- */
-
-TEST(isInt, is0){
-  char *tmp = "0";
-  ASSERT_TRUE(isInt(tmp));
-}
-TEST(isInt, is10){
-  char *tmp = "10";
-  ASSERT_TRUE(isInt(tmp));
-}
-TEST(isInt, isN10){
-  char *tmp = "-10";
-  ASSERT_TRUE(isInt(tmp));
-}
-TEST(isInt, isA10){
-  char *tmp = "A10";
-  ASSERT_FALSE(isInt(tmp));
-}
-TEST(isInt, isFloat){
-  char *tmp = "10.10";
-  ASSERT_FALSE(isInt(tmp));
+    // Test invalid file name
+    char* invalidFileName = "test.txt";
+    checkAOPFile(invalidFileName);
+    ASSERT_TRUE(EXIT_FAILURE);
 }
 
 /*
- *  isBinary
+ *  checkLineSize
  */
-
-TEST(isBinary, is0b1010){
-  char *tmp = "0b1010";
-  ASSERT_TRUE(isBinary(tmp));
-}
-TEST(isBinary, is0B1){
-  char *tmp = "0B1";
-  ASSERT_FALSE(isBinary(tmp));
-}
-TEST(isBinary, is0x1010){
-  char *tmp = "0x1010";
-  ASSERT_FALSE(isBinary(tmp));
-}
-TEST(isBinary, is0b1012){
-  char *tmp = "0b1012";
-  ASSERT_FALSE(isBinary(tmp));
-}
-TEST(isBinary, is0b1010101010101010101010101010){
-  char *tmp = "0b1010101010101010101010101010";
-  ASSERT_TRUE(isBinary(tmp));
-}
-TEST(isBinary, is1010){
-  char *tmp = "1010";
-  ASSERT_FALSE(isBinary(tmp));
-}
 
 /*
- *  isOctal
+ *  flagsSet
  */
 
-TEST(isOctal, is0o1010){
-  char *tmp = "0o1010";
-  ASSERT_TRUE(isOctal(tmp));
+/*
+ *  getIfArgs
+ */
+
+/*
+ *  getInst
+ */
+
+/*
+ *  getInstArgs
+ */
+
+/*
+ *  isCmp
+ */
+
+/*
+ *  isDecla
+ */
+
+TEST(isDecla, lab){
+    // Test valid declaration keyword
+    char *tmp = "lab";
+    instNode_t *newNode = (instNode_t *)malloc(sizeof(instNode_t));
+    bool test = isDecla(tmp, newNode);
+    ASSERT_TRUE(test);
 }
-TEST(isOctal, is0O7){
-  char *tmp = "0O7";
-  ASSERT_FALSE(isOctal(tmp));
+
+TEST(isDecla, labWithSymbol){
+    // Test invalid declaration with symbol
+    char *tmp = "lab:";
+    instNode_t *newNode = (instNode_t *)malloc(sizeof(instNode_t));
+    bool test = isDecla(tmp, newNode);
+    ASSERT_FALSE(test);
 }
-TEST(isOctal, is0x1010){
-  char *tmp = "0x1010";
-  ASSERT_FALSE(isOctal(tmp));
-}
-TEST(isOctal, is0o0369){
-  char *tmp = "0o0369";
-  ASSERT_FALSE(isOctal(tmp));
-}
-TEST(isOctal, is0o0123456701234567){
-  char *tmp = "0o0123456701234567";
-  ASSERT_TRUE(isOctal(tmp));
-}
-TEST(isOctal, is7070){
-  char *tmp = "7070";
-  ASSERT_FALSE(isOctal(tmp));
+
+TEST(isDecla, var){
+    // Test valid declaration keyword
+    char *tmp = "var";
+    instNode_t *newNode = (instNode_t *)malloc(sizeof(instNode_t));
+    bool test = isDecla(tmp, newNode);
+    ASSERT_TRUE(test);
 }
 
 /*
@@ -149,87 +79,305 @@ TEST(isOctal, is7070){
  */
 
 TEST(isHex, is0x0Af){
-  char *tmp = "0x0Af";
-  ASSERT_TRUE(isHex(tmp));
+    // Test hexadecimal number with prefix "0x"
+    char *tmp = "0x0Af";
+    ASSERT_TRUE(isHex(tmp));
 }
-TEST(isHex, is0XF){
-  char *tmp = "0XF";
-  ASSERT_FALSE(isHex(tmp));
-}
-TEST(isHex, is0x1010){
-  char *tmp = "0bFFF";
-  ASSERT_FALSE(isHex(tmp));
-}
-TEST(isHex, is0x0AFG){
-  char *tmp = "0x0AFG";
-  ASSERT_FALSE(isHex(tmp));
-}
+
 TEST(isHex, is0x09AF){
-  char *tmp = "0x09AF";
-  ASSERT_TRUE(isHex(tmp));
+    // Test valid hexadecimal number
+    char *tmp = "0x09AF";
+    ASSERT_TRUE(isHex(tmp));
 }
+
+TEST(isHex, is0x0AFG){
+    // Test invalid hexadecimal number
+    char *tmp = "0x0AFG";
+    ASSERT_FALSE(isHex(tmp));
+}
+
+TEST(isHex, is0x1010){
+    // Test binary number with prefix "0b"
+    char *tmp = "0bFFF";
+    ASSERT_FALSE(isHex(tmp));
+}
+
+TEST(isHex, is0XF){
+    // Test hexadecimal number with uppercase prefix "0X"
+    char *tmp = "0XF";
+    ASSERT_FALSE(isHex(tmp));
+}
+
 TEST(isHex, is09aF){
-  char *tmp = "09aF";
-  ASSERT_FALSE(isHex(tmp));
+    // Test invalid hexadecimal number without prefix
+    char *tmp = "09aF";
+    ASSERT_FALSE(isHex(tmp));
 }
 
 /*
- *  isFloat
+ *  isInt
  */
 
-TEST(isFloat, is10){
-  char *tmp = "10";
-  ASSERT_FALSE(isFloat(tmp));
+TEST(isInt, is0){
+    // Test integer 0
+    char *tmp = "0";
+    ASSERT_TRUE(isInt(tmp));
 }
-TEST(isFloat, is10FLoat){
-  char *tmp = "10.0";
-  ASSERT_TRUE(isFloat(tmp));
+
+TEST(isInt, is10){
+    // Test positive integer
+    char *tmp = "10";
+    ASSERT_TRUE(isInt(tmp));
 }
-TEST(isFloat, is0Float){
-  char *tmp = "0.1234";
-  ASSERT_TRUE(isFloat(tmp));
+
+TEST(isInt, isA10){
+    // Test non-integer input
+    char *tmp = "A10";
+    ASSERT_FALSE(isInt(tmp));
 }
-TEST(isFloat, isDotFirst){
-  char *tmp = ".1234";
-  ASSERT_FALSE(isFloat(tmp));
+
+TEST(isInt, isFloat){
+    // Test floating-point input
+    char *tmp = "10.10";
+    ASSERT_FALSE(isInt(tmp));
 }
-TEST(isFloat, DoubleDot){
-  char *tmp = "0.1.234";
-  ASSERT_FALSE(isFloat(tmp));
+
+TEST(isInt, isN10){
+    // Test negative integer
+    char *tmp = "-10";
+    ASSERT_TRUE(isInt(tmp));
 }
-TEST(isFloat, SignedFloat){
-  char *tmp = "-0.1234";
-  ASSERT_TRUE(isFloat(tmp));
+
+/*
+ *  isOctal
+ */
+
+TEST(isOctal, is0O7){
+    // Test octal number with uppercase prefix "0O"
+    char *tmp = "0O7";
+    ASSERT_FALSE(isOctal(tmp));
+}
+
+TEST(isOctal, is0o0123456701234567){
+    // Test long octal number
+    char *tmp = "0o0123456701234567";
+    ASSERT_TRUE(isOctal(tmp));
+}
+
+TEST(isOctal, is0o0369){
+    // Test invalid octal number
+    char *tmp = "0o0369";
+    ASSERT_FALSE(isOctal(tmp));
+}
+
+TEST(isOctal, is0o1010){
+    // Test octal number with prefix "0o"
+    char *tmp = "0o1010";
+    ASSERT_TRUE(isOctal(tmp));
+}
+
+TEST(isOctal, is7070){
+    // Test decimal number
+    char *tmp = "7070";
+    ASSERT_FALSE(isOctal(tmp));
+}
+
+TEST(isOctal, is0x1010){
+    // Test hexadecimal number with prefix "0x"
+    char *tmp = "0x1010";
+    ASSERT_FALSE(isOctal(tmp));
+}
+
+/*
+ *  isOp
+ */
+
+TEST(isOp, add){
+    // Test valid operator name
+    char *tmp = "add";
+    instNode_t *newNode = (instNode_t *)malloc(sizeof(instNode_t));
+    bool test = isOp(tmp, newNode);
+    ASSERT_TRUE(test);
+}
+
+TEST(isOp, addSymbols){
+    // Test valid operator symbol
+    char *tmp = "+";
+    instNode_t *newNode = (instNode_t *)malloc(sizeof(instNode_t));
+    bool test = isOp(tmp, newNode);
+    ASSERT_TRUE(test);
+}
+
+TEST(isOp, GOTO){
+    // Test invalid operator name
+    char *tmp = "goto";
+    instNode_t *newNode = (instNode_t *)malloc(sizeof(instNode_t));
+    bool test = isOp(tmp, newNode);
+    ASSERT_FALSE(test);
+}
+
+TEST(isOp, incSymbols){
+    // Test invalid operator symbol
+    char *tmp = "++";
+    instNode_t *newNode = (instNode_t *)malloc(sizeof(instNode_t));
+    bool test = isOp(tmp, newNode);
+    ASSERT_FALSE(test);
+}
+
+TEST(isOp, moduloSymbol){
+    // Test valid operator symbol
+    char *tmp = "%";
+    instNode_t *newNode = (instNode_t *)malloc(sizeof(instNode_t));
+    bool test = isOp(tmp, newNode);
+    ASSERT_TRUE(test);
+}
+
+/*
+ *  isAct
+ */
+
+TEST(isAct, addSymbols){
+    // Test invalid action symbol
+    char *tmp = "+";
+    instNode_t *newNode = (instNode_t *)malloc(sizeof(instNode_t));
+    bool test = isAct(tmp, newNode);
+    ASSERT_FALSE(test);
+}
+
+TEST(isAct, GOTO){
+    // Test valid action name
+    char *tmp = "goto";
+    instNode_t *newNode = (instNode_t *)malloc(sizeof(instNode_t));
+    bool test = isAct(tmp, newNode);
+    ASSERT_TRUE(test);
+}
+
+TEST(isAct, incSymbols){
+    // Test invalid action symbol
+    char *tmp = "++";
+    instNode_t *newNode = (instNode_t *)malloc(sizeof(instNode_t));
+    bool test = isAct(tmp, newNode);
+    ASSERT_FALSE(test);
+}
+
+TEST(isAct, moduloSymbol){
+    // Test invalid action symbol
+    char *tmp = "%";
+    instNode_t *newNode = (instNode_t *)malloc(sizeof(instNode_t));
+    bool test = isAct(tmp, newNode);
+    ASSERT_FALSE(test);
+}
+
+TEST(isAct, ob1){
+    // Test valid action name
+    char *tmp = "ob1";
+    instNode_t *newNode = (instNode_t *)malloc(sizeof(instNode_t));
+    bool test = isAct(tmp, newNode);
+    ASSERT_TRUE(test);
+}
+
+TEST(isAct, ob12){
+    // Test invalid action name
+    char *tmp = "ob12";
+    instNode_t *newNode = (instNode_t *)malloc(sizeof(instNode_t));
+    bool test = isAct(tmp, newNode);
+    ASSERT_FALSE(test);
+}
+
+/*
+ *  isBinary
+ */
+
+TEST(isBinary, is0b1010){
+    // Test binary number with prefix "0b"
+    char *tmp = "0b1010";
+    ASSERT_TRUE(isBinary(tmp));
+}
+
+TEST(isBinary, is0b1010101010101010101010101010){
+    // Test long binary number
+    char *tmp = "0b1010101010101010101010101010";
+    ASSERT_TRUE(isBinary(tmp));
+}
+
+TEST(isBinary, is0b1012){
+    // Test invalid binary number
+    char *tmp = "0b1012";
+    ASSERT_FALSE(isBinary(tmp));
+}
+
+TEST(isBinary, is1010){
+    // Test decimal number
+    char *tmp = "1010";
+    ASSERT_FALSE(isBinary(tmp));
+}
+
+TEST(isBinary, is0B1){
+    // Test binary number with uppercase prefix "0B"
+    char *tmp = "0B1";
+    ASSERT_FALSE(isBinary(tmp));
+}
+
+TEST(isBinary, is0x1010){
+    // Test hexadecimal number with prefix "0x"
+    char *tmp = "0x1010";
+    ASSERT_FALSE(isBinary(tmp));
 }
 
 /*
  *  isChar
  */
 
-TEST(isChar, isString){
-  char *tmp = "\"a\"";
-  ASSERT_FALSE(isChar(tmp));
-}
 TEST(isChar, isChar){
-  char *tmp = "'a'";
-  ASSERT_TRUE(isChar(tmp));
+    // Test single character enclosed in single quotes
+    char *tmp = "'a'";
+    ASSERT_TRUE(isChar(tmp));
 }
+
 TEST(isChar, isNotChar){
-  char *tmp = "'abcdef'"; // Doubt for this one (should concider a as char and ignore the rest)
-  ASSERT_FALSE(isChar(tmp));
+    // Test invalid single character format
+    char *tmp = "'abcdef'";
+    ASSERT_FALSE(isChar(tmp)); // Doubt for this one (should consider 'a' as a character and ignore the rest)
+}
+
+TEST(isChar, isString){
+    // Test string enclosed in double quotes
+    char *tmp = "\"a\"";
+    ASSERT_FALSE(isChar(tmp));
 }
 
 /*
- *  isString
+ *  isFloat
  */
 
-TEST(isString, isString){
-  char *tmp = "\"a\"";
-  ASSERT_TRUE(isString(tmp));
+TEST(isFloat, DoubleDot){
+    // Test invalid float number with double decimal points
+    char *tmp = "0.1.234";
+    ASSERT_FALSE(isFloat(tmp));
 }
-TEST(isString, isChar){
-  char *tmp = "'a'";
-  ASSERT_FALSE(isString(tmp));
+
+TEST(isFloat, SignedFloat){
+    // Test signed float number
+    char *tmp = "-0.1234";
+    ASSERT_TRUE(isFloat(tmp));
+}
+
+TEST(isFloat, is0Float){
+    // Test float number without leading integer
+    char *tmp = ".1234";
+    ASSERT_FALSE(isFloat(tmp));
+}
+
+TEST(isFloat, is10){
+    // Test integer number
+    char *tmp = "10";
+    ASSERT_TRUE(isFloat(tmp));
+}
+
+TEST(isFloat, is10FLoat){
+    // Test float number with decimal point
+    char *tmp = "10.0";
+    ASSERT_TRUE(isFloat(tmp));
 }
 
 /*
@@ -237,16 +385,37 @@ TEST(isString, isChar){
  */
 
 TEST(isReg, rg0){
-  char *tmp = "rg0";
-  ASSERT_TRUE(isReg(tmp));
+    // Test valid register name
+    char *tmp = "rg0";
+    ASSERT_TRUE(isReg(tmp));
 }
-TEST(isReg, RG0){
-  char *tmp = "RG0";
-  ASSERT_FALSE(isReg(tmp));
-}
+
 TEST(isReg, rg8){
-  char *tmp = "rg8";
-  ASSERT_FALSE(isReg(tmp));
+    // Test invalid register name with invalid number
+    char *tmp = "rg8";
+    ASSERT_FALSE(isReg(tmp));
+}
+
+TEST(isReg, RG0){
+    // Test invalid register name with uppercase
+    char *tmp = "RG0";
+    ASSERT_FALSE(isReg(tmp));
+}
+
+/*
+ *  isString
+ */
+
+TEST(isString, isChar){
+    // Test single character enclosed in single quotes
+    char *tmp = "'a'";
+    ASSERT_FALSE(isString(tmp));
+}
+
+TEST(isString, isString){
+    // Test string enclosed in double quotes
+    char *tmp = "\"a\"";
+    ASSERT_TRUE(isString(tmp));
 }
 
 /*
@@ -254,17 +423,67 @@ TEST(isReg, rg8){
  */
 
 TEST(isTarget, allCharacters){
-  char *tmp = "azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN1234567890_\0";  // Is it ok to start or finish by _ ?
-  ASSERT_TRUE(isTarget(tmp));
+    // Test valid target name with all valid characters
+    char *tmp = "azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN1234567890_";
+    ASSERT_TRUE(isTarget(tmp));
 }
-TEST(isTarget, SpecialCharacters){
-  char *tmp = "&";
-  ASSERT_FALSE(isTarget(tmp));
-}
+
 TEST(isTarget, rtn){
-  char *tmp = "\n";
-  ASSERT_FALSE(isTarget(tmp));
+    // Test invalid target name with newline character
+    char *tmp = "\n";
+    ASSERT_FALSE(isTarget(tmp));
 }
+
+TEST(isTarget, SpecialCharacters){
+    // Test invalid target name with special characters
+    char *tmp = "&";
+    ASSERT_FALSE(isTarget(tmp));
+}
+
+/*
+ *  parseArgs
+ */
+
+TEST(parseArgs, help) {
+    // Test help flag
+    char *argv[] = {"test", "-h"};
+    int argc = sizeof(argv) / sizeof(argv[0]);
+    flags_t flags = parseArgs(argc, argv);
+    ASSERT_TRUE(flags.help);
+}
+
+TEST(parseArgs, version) {
+    // Test version flag
+    char *argv[] = {"test", "-v"};
+    int argc = sizeof(argv) / sizeof(argv[0]);
+    flags_t flags = parseArgs(argc, argv);
+    ASSERT_TRUE(flags.version);
+}
+
+TEST(parseArgs, verbose) {
+    // Test verbose flag
+    char *argv[] = {"test", "-V"};
+    int argc = sizeof(argv) / sizeof(argv[0]);
+    flags_t flags = parseArgs(argc, argv);
+    ASSERT_TRUE(flags.verbose);
+}
+
+TEST(parseArgs, debug) {
+    // Test debug flag
+    char *argv[] = {"test", "-d"};
+    int argc = sizeof(argv) / sizeof(argv[0]);
+    flags_t flags = parseArgs(argc, argv);
+    ASSERT_TRUE(flags.debug);
+}
+
+TEST(parseArgs, invalidFlag) {
+    // Test false argv
+    char *argv[] = {"iat2.exe", "-f"};
+    int argc = sizeof(argv) / sizeof(argv[0]);
+    flags_t flags = parseArgs(argc, argv);
+    ASSERT_FALSE(flags.help);
+}
+
 
 
 
