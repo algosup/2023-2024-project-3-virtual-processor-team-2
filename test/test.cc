c2cfa7299b16da2b5846bd887913883087031116
Maxime CARON <145995231+MaximeAlgosup@users.noreply.github.com>
2024-02-08 16:53:33 +0100

    Refactor test order

--- test/test.cc
+++ test/test.cc
@@ -12,78 +12,15 @@
 #include "builder.h"
 
 /*
- *  parseArgs
- */
-
-// Test help flag
-TEST(parseArgs, help) {
-char *argv[] = {const_cast<char *>("test"), const_cast<char *>("-h")};
-  int argc = sizeof(argv) / sizeof(argv[0]);
-  flags_t flags = parseArgs(argc, argv);
-  ASSERT_TRUE(flags.help);
-}
-// Test version flag
-TEST(parseArgs, version) {
-  char *argv[] = {const_cast<char *>("test"), const_cast<char *>("-v")};
-  int argc = sizeof(argv) / sizeof(argv[0]);
-  flags_t flags = parseArgs(argc, argv);
-  ASSERT_TRUE(flags.version);
-}
-// Test verbose flag
-TEST(parseArgs, verbose) {
-  char *argv[] = {const_cast<char *>("test"), const_cast<char *>("-V")};
-  int argc = sizeof(argv) / sizeof(argv[0]);
-  flags_t flags = parseArgs(argc, argv);
-  ASSERT_TRUE(flags.verbose);
-}
-// Test debug flag
-TEST(parseArgs, debug) {
-  char *argv[] = {const_cast<char *>("test"), const_cast<char *>("-d")};
-  int argc = sizeof(argv) / sizeof(argv[0]);
-  flags_t flags = parseArgs(argc, argv);
-  ASSERT_TRUE(flags.debug);
-}
-// Test false argv
-TEST(parseArgs, invalidFlag) {
-  char *argv[] = {const_cast<char *>("test"), const_cast<char *>("-a")};
-  int argc = sizeof(argv) / sizeof(argv[0]);
-  flags_t flags = parseArgs(argc, argv);
-  ASSERT_FALSE(flags.help);
-}
-
-/*
  *  checkAOPFile
  */
 
 // this test won't display a message because the test just above already deals with the same function.
 TEST(checkAOPFile, invalidFileName) {
+  // Test invalid file name
   char* invalidFileName = const_cast<char *>("test.txt"); // Replace fileName with an invalid file name
   checkAOPFile(invalidFileName);
   ASSERT_TRUE(EXIT_FAILURE);
-}
-/*
- *  isInt
- */
-
-TEST(isInt, is0){
-  char *tmp = const_cast<char *>("0");
-  ASSERT_TRUE(isInt(tmp));
-}
-TEST(isInt, is10){
-  char *tmp = const_cast<char *>("10");
-  ASSERT_TRUE(isInt(tmp));
-}
-TEST(isInt, isN10){
-  char *tmp = const_cast<char *>("-10");
-  ASSERT_TRUE(isInt(tmp));
-}
-TEST(isInt, isA10){
-  char *tmp = const_cast<char *>("A10");
-  ASSERT_FALSE(isInt(tmp));
-}
-TEST(isInt, isFloat){
-  char *tmp = const_cast<char *>("10.10");
-  ASSERT_FALSE(isInt(tmp));
 }
 
 /*
@@ -91,110 +28,34 @@ TEST(isInt, isFloat){
  */
 
 TEST(isBinary, is0b1010){
+  // Test binary number with prefix "0b"
   char *tmp = const_cast<char *>("0b1010");
   ASSERT_TRUE(isBinary(tmp));
 }
 TEST(isBinary, is0B1){
+  // Test long binary number
   char *tmp = const_cast<char *>("0B1");
   ASSERT_FALSE(isBinary(tmp));
 }
 TEST(isBinary, is0x1010){
+  // Test invalid binary number
   char *tmp = const_cast<char *>("0x1010");
   ASSERT_FALSE(isBinary(tmp));
 }
 TEST(isBinary, is0b1012){
+  // Test decimal number
   char *tmp = const_cast<char *>("0b1012");
   ASSERT_FALSE(isBinary(tmp));
 }
 TEST(isBinary, is0b1010101010101010101010101010){
+  // Test binary number with uppercase prefix "0B"
   char *tmp = const_cast<char *>("0b1010101010101010101010101010");
   ASSERT_TRUE(isBinary(tmp));
 }
 TEST(isBinary, is1010){
+  // Test hexadecimal number with prefix "0x"
   char *tmp = const_cast<char *>("1010");
   ASSERT_FALSE(isBinary(tmp));
-}
-
-/*
- *  isOctal
- */
-
-TEST(isOctal, is0o1010){
-  char *tmp = const_cast<char *>("0o1010");
-  ASSERT_TRUE(isOctal(tmp));
-}
-TEST(isOctal, is0O7){
-  char *tmp = const_cast<char *>("0O7");
-  ASSERT_FALSE(isOctal(tmp));
-}
-TEST(isOctal, is0x1010){
-  char *tmp = const_cast<char *>("0x1010");
-  ASSERT_FALSE(isOctal(tmp));
-}
-TEST(isOctal, is0o0369){
-  char *tmp = const_cast<char *>("0o0369");
-  ASSERT_FALSE(isOctal(tmp));
-}
-TEST(isOctal, is0o0123456701234567){
-  char *tmp = const_cast<char *>("0o0123456701234567");
-  ASSERT_TRUE(isOctal(tmp));
-}
-TEST(isOctal, is7070){
-  char *tmp = const_cast<char *>("7070");
-  ASSERT_FALSE(isOctal(tmp));
-}
-
-/*
- *  isHex
- */
-
-TEST(isHex, is0x0Af){
-  char *tmp = const_cast<char *>("0x0Af");
-  ASSERT_TRUE(isHex(tmp));
-}
-TEST(isHex, is0XF){
-  char *tmp = const_cast<char *>("0XF");
-  ASSERT_FALSE(isHex(tmp));
-}
-TEST(isHex, is0x1010){
-  char *tmp = const_cast<char *>("0bFFF");
-  ASSERT_FALSE(isHex(tmp));
-}
-TEST(isHex, is0x0AFG){
-  char *tmp = const_cast<char *>("0x0AFG");
-  ASSERT_FALSE(isHex(tmp));
-}
-TEST(isHex, is0x09AF){
-  char *tmp = const_cast<char *>("0x09AF");
-  ASSERT_TRUE(isHex(tmp));
-}
-TEST(isHex, is09aF){
-  char *tmp = const_cast<char *>("09aF");
-  ASSERT_FALSE(isHex(tmp));
-}
-
-/*
- *  isFloat
- */
-TEST(isFloat, is10FLoat){
-  char *tmp = const_cast<char *>("10.0");
-  ASSERT_TRUE(isFloat(tmp));
-}
-TEST(isFloat, is0Float){
-  char *tmp = const_cast<char *>("0.1234");
-  ASSERT_TRUE(isFloat(tmp));
-}
-TEST(isFloat, isDotFirst){
-  char *tmp = const_cast<char *>(".1234");
-  ASSERT_FALSE(isFloat(tmp));
-}
-TEST(isFloat, DoubleDot){
-  char *tmp = const_cast<char *>("0.1.234");
-  ASSERT_FALSE(isFloat(tmp));
-}
-TEST(isFloat, SignedFloat){
-  char *tmp = const_cast<char *>("-0.1234");
-  ASSERT_TRUE(isFloat(tmp));
 }
 
 /*
@@ -202,29 +63,148 @@ TEST(isFloat, SignedFloat){
  */
 
 TEST(isChar, isString){
+  // Test single character enclosed in single quotes
   char *tmp = const_cast<char *>("\"a\"");
   ASSERT_FALSE(isChar(tmp));
 }
 TEST(isChar, isChar){
+  // Test invalid single character format
   char *tmp = const_cast<char *>("'a'");
   ASSERT_TRUE(isChar(tmp));
 }
 TEST(isChar, isNotChar){
+  // Test string enclosed in double quotes
   char *tmp = const_cast<char *>("'abcdef'"); // Doubt for this one (should concider a as char and ignore the rest)
   ASSERT_FALSE(isChar(tmp));
 }
 
 /*
- *  isString
+ *  isFloat
+ */
+TEST(isFloat, is10FLoat){
+  // Test float number with decimal point
+  char *tmp = const_cast<char *>("10.0");
+  ASSERT_TRUE(isFloat(tmp));
+}
+TEST(isFloat, is0Float){
+  // Test float number with leading zero
+  char *tmp = const_cast<char *>("0.1234");
+  ASSERT_TRUE(isFloat(tmp));
+}
+TEST(isFloat, isDotFirst){
+  // Test float number without leading integer
+  char *tmp = const_cast<char *>(".1234");
+  ASSERT_FALSE(isFloat(tmp));
+}
+TEST(isFloat, DoubleDot){
+  // Test invalid float number with double decimal points
+  char *tmp = const_cast<char *>("0.1.234");
+  ASSERT_FALSE(isFloat(tmp));
+}
+TEST(isFloat, SignedFloat){
+  // Test signed float number
+  char *tmp = const_cast<char *>("-0.1234");
+  ASSERT_TRUE(isFloat(tmp));
+}
+
+/*
+ *  isHex
  */
 
-TEST(isString, isString){
-  char *tmp = const_cast<char *>("\"a\"");
-  ASSERT_TRUE(isString(tmp));
+TEST(isHex, is0x0Af){
+  // Test hexadecimal number with prefix "0x"
+  char *tmp = const_cast<char *>("0x0Af");
+  ASSERT_TRUE(isHex(tmp));
 }
-TEST(isString, isChar){
-  char *tmp = const_cast<char *>("'a'");
-  ASSERT_FALSE(isString(tmp));
+TEST(isHex, is0XF){
+  // Test hexadecimal number with uppercase prefix "0X"
+  char *tmp = const_cast<char *>("0XF");
+  ASSERT_FALSE(isHex(tmp));
+}
+TEST(isHex, is0x1010){
+  // Test binary number with prefix "0b"
+  char *tmp = const_cast<char *>("0bFFF");
+  ASSERT_FALSE(isHex(tmp));
+}
+TEST(isHex, is0x0AFG){
+  // Test invalid hexadecimal number
+  char *tmp = const_cast<char *>("0x0AFG");
+  ASSERT_FALSE(isHex(tmp));
+}
+TEST(isHex, is0x09AF){
+  // Test valid hexadecimal number
+  char *tmp = const_cast<char *>("0x09AF");
+  ASSERT_TRUE(isHex(tmp));
+}
+TEST(isHex, is09aF){
+  // Test invalid hexadecimal number without prefix
+  char *tmp = const_cast<char *>("09aF");
+  ASSERT_FALSE(isHex(tmp));
+}
+
+/*
+ *  isInt
+ */
+
+TEST(isInt, is0){
+  // Test integer 0
+  char *tmp = const_cast<char *>("0");
+  ASSERT_TRUE(isInt(tmp));
+}
+TEST(isInt, is10){
+  // Test positive integer
+  char *tmp = const_cast<char *>("10");
+  ASSERT_TRUE(isInt(tmp));
+}
+TEST(isInt, isN10){
+  // Test negative integer
+  char *tmp = const_cast<char *>("-10");
+  ASSERT_TRUE(isInt(tmp));
+}
+TEST(isInt, isA10){
+  // Test non-integer input
+  char *tmp = const_cast<char *>("A10");
+  ASSERT_FALSE(isInt(tmp));
+}
+TEST(isInt, isFloat){
+  // Test floating-point input
+  char *tmp = const_cast<char *>("10.10");
+  ASSERT_FALSE(isInt(tmp));
+}
+
+/*
+ *  isOctal
+ */
+
+TEST(isOctal, is0o1010){
+  // Test octal number with prefix "0o"
+  char *tmp = const_cast<char *>("0o1010");
+  ASSERT_TRUE(isOctal(tmp));
+}
+TEST(isOctal, is0O7){
+  // Test octal number with uppercase prefix "0O"
+  char *tmp = const_cast<char *>("0O7");
+  ASSERT_FALSE(isOctal(tmp));
+}
+TEST(isOctal, is0x1010){
+  // Test hexadecimal number with prefix "0x"
+  char *tmp = const_cast<char *>("0x1010");
+  ASSERT_FALSE(isOctal(tmp));
+}
+TEST(isOctal, is0o0369){
+  // Test invalid octal number
+  char *tmp = const_cast<char *>("0o0369");
+  ASSERT_FALSE(isOctal(tmp));
+}
+TEST(isOctal, is0o0123456701234567){
+  // Test long octal number
+  char *tmp = const_cast<char *>("0o0123456701234567");
+  ASSERT_TRUE(isOctal(tmp));
+}
+TEST(isOctal, is7070){
+  // Test decimal number
+  char *tmp = const_cast<char *>("7070");
+  ASSERT_FALSE(isOctal(tmp));
 }
 
 /*
@@ -232,16 +212,34 @@ TEST(isString, isChar){
  */
 
 TEST(isReg, rg0){
+  // Test valid register name
   char *tmp = const_cast<char *>("rg0");
   ASSERT_TRUE(isReg(tmp));
 }
 TEST(isReg, RG0){
+  // Test invalid register name with uppercase
   char *tmp = const_cast<char *>("RG0");
   ASSERT_FALSE(isReg(tmp));
 }
 TEST(isReg, rg8){
+  // Test invalid register name with invalid number
   char *tmp = const_cast<char *>("rg8");
   ASSERT_FALSE(isReg(tmp));
+}
+
+/*
+ *  isString
+ */
+
+TEST(isString, isString){
+  // Test string enclosed in double quotes
+  char *tmp = const_cast<char *>("\"a\"");
+  ASSERT_TRUE(isString(tmp));
+}
+TEST(isString, isChar){
+  // Test single character enclosed in single quotes
+  char *tmp = const_cast<char *>("'a'");
+  ASSERT_FALSE(isString(tmp));
 }
 
 /*
@@ -249,19 +247,61 @@ TEST(isReg, rg8){
  */
 
 TEST(isTarget, allCharacters){
+  // Test valid target name with all valid characters
   char *tmp = const_cast<char *>("azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN1234567890_\0");  // Is it ok to start or finish by _ ?
   ASSERT_TRUE(isTarget(tmp));
 }
 TEST(isTarget, SpecialCharacters){
+  // Test invalid target name with special characters
   char *tmp = const_cast<char *>("&");
   ASSERT_FALSE(isTarget(tmp));
 }
 TEST(isTarget, rtn){
+  // Test invalid target name with newline character
   char *tmp = const_cast<char *>("\n");
   ASSERT_FALSE(isTarget(tmp));
 }
 
+/*
+ *  parseArgs
+ */
 
+TEST(parseArgs, help) {
+  // Test help flag
+  char *argv[] = {const_cast<char *>("test"), const_cast<char *>("-h")};
+  int argc = sizeof(argv) / sizeof(argv[0]);
+  flags_t flags = parseArgs(argc, argv);
+  ASSERT_TRUE(flags.help);
+}
+TEST(parseArgs, version) {
+  // Test version flag
+  char *argv[] = {const_cast<char *>("test"), const_cast<char *>("-v")};
+  int argc = sizeof(argv) / sizeof(argv[0]);
+  flags_t flags = parseArgs(argc, argv);
+  ASSERT_TRUE(flags.version);
+}
+TEST(parseArgs, verbose) {
+  // Test verbose flag
+  char *argv[] = {const_cast<char *>("test"), const_cast<char *>("-V")};
+  int argc = sizeof(argv) / sizeof(argv[0]);
+  flags_t flags = parseArgs(argc, argv);
+  ASSERT_TRUE(flags.verbose);
+}
+TEST(parseArgs, debug) {
+  // Test debug flag
+  char *argv[] = {const_cast<char *>("test"), const_cast<char *>("-d")};
+  int argc = sizeof(argv) / sizeof(argv[0]);
+  flags_t flags = parseArgs(argc, argv);
+  ASSERT_TRUE(flags.debug);
+}
+
+TEST(parseArgs, invalidFlag) {
+  // Test false argv
+  char *argv[] = {const_cast<char *>("test"), const_cast<char *>("-a")};
+  int argc = sizeof(argv) / sizeof(argv[0]);
+  flags_t flags = parseArgs(argc, argv);
+  ASSERT_FALSE(flags.help);
+}
 
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
