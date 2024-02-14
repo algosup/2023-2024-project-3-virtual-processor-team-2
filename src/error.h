d313dd2349419d421fa2d121193a9b3410c2b9f5
PaulNowak36 <91249965+PaulNowak36@users.noreply.github.com>
2024-02-14 09:04:30 +0100

    error library updated

--- src/error.h
+++ src/error.h
@@ -24,7 +24,7 @@ error_t *initErrorFile(const char* out, char *inputFile);
 // ---------- Parsing/ Assembling errors ----------
 
 /*
-    Throw an error when the given instruction doesn't exist
+    Throw an error whrn the given instruction doesn't exist
     params:
         inst: Instruction
         node: Instruction node to get additionnal information
@@ -50,8 +50,18 @@ void errorLineSize(long lineNb, const char* out, error_t *errData);
 */
 void printErrorSummary(error_t *errData);
 
+// ---------- Bin converting errors ----------
+
 // ---------- Fatal errors ----------
 
+/*
+    Throw an error when the file contains errors 
+    params:
+        filename: File name
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorIssues(char* filename, const char* out, error_t *errData);
 
 /*
     Throw an error when the file is not found
@@ -65,45 +75,22 @@ void errorfnf(char* filename, const char* out, error_t *errData);
 /*
     Throw an error when the file extension is invalid
     params:
-        filename: File name
+        filename: File's name
         out: Target file (can be NULL)
         errData: Error history
 */
 void errorInvalidExt(char* filename, const char* out, error_t *errData);
 
 
-
-
-
 /*
-    Throw an error when there is too many arguments
+    Display a file error with error messages depending on the issue
     params:
-        out: Target file (can be NULL)
-        errData: Error history
-*/
-void errorTooManyArg(const char* out, error_t *errData);
-
-/*
-    Throw an error when there is no argument
-    params:
-        out: Target file (can be NULL)
-        errData: Error history
-*/
-void errorNoArg(const char* out, error_t *errData);
-
-/*
-    Throw an error when there is a special character missing
-    params:
-        lineNb: line number
         filename: File name
         out: Target file (can be NULL)
         errData: Error history
+        errorNumber: Type of error
 */
-void errorSpeCharMiss(long lineNb, const char* out, error_t *errData, char* filename)
-
-/*
-
-*/
+void displayFileError(char* filename, const char* out, error_t *errData, int errorNumber);
 
 #ifdef __cplusplus
 }
