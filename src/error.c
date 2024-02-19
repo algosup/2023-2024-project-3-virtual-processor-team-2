bb3766047dd99bc7b4304771158b0bb8077fddef
Guillaume DERAMCHI <guillaume.deramchi@algosup.com>
2024-02-19 14:42:09 +0100

    Finished error implementation
    Implemented all the error for Paul to test them.

--- src/error.c
+++ src/error.c
@@ -75,6 +75,9 @@ void displayError(const char *errType, const char *errDetails, char *errLocation
     }
 }
 
+
+// ---------------------------PARSER ERROR--------------------------------------
+
 void errorNoArg(const char *out, error_t *errData){
     char *errType = "Syntax Error";
     char errDetails[64];
@@ -128,6 +131,258 @@ void errorInvalidExt(char *filename, const char *out, error_t *errData){
     displayError(errType, errDetails, NULL, out, errData);
     printErrorSummary(errData); 
     exit(EXIT_FAILURE); 
+}
+
+
+void errorInstructionMissing(long lineNb, const char *out, error_t *errData){
+    char *errType = "Syntax Error";
+    char errDetails[64];
+    char errLocation[64];
+
+    sprintf(errDetails, "No instruction found in the input line");
+    sprintf(errLocation, "File %s, line %ld", errData->inputFile, lineNb);
+
+    displayError(errType, errDetails, errLocation, out, errData);
+}
+
+
+void errorInvalidRegister(char *reg, long lineNb, const char *out, error_t *errData){
+    char *errType = "Register Error";
+    char errDetails[64];
+    char errLocation[64];
+
+    sprintf(errDetails, "Invalid register '%s'", reg);
+    sprintf(errLocation, "File %s, line %ld", errData->inputFile, lineNb);
+
+    displayError(errType, errDetails, errLocation, out, errData);
+}
+
+
+void errorUnexpectedSymbol(char symbol, long lineNb, const char *out, error_t *errData){
+    char *errType = "Syntax Error";
+    char errDetails[64];
+    char errLocation[64];
+
+    sprintf(errDetails, "'%c' is unexpected", symbol);
+    sprintf(errLocation, "File %s, line %ld", errData->inputFile, lineNb);
+
+    displayError(errType, errDetails, errLocation, out, errData);
+}
+
+
+void errorMissingSymbol(char symbol, long lineNb, const char *out, error_t *errData){
+    char *errType = "Syntax Error";
+    char errDetails[64];
+    char errLocation[64];
+
+    sprintf(errDetails, "'%c' is missing between arguments", symbol);
+    sprintf(errLocation, "File %s, line %ld", errData->inputFile, lineNb);
+
+    displayError(errType, errDetails, errLocation, out, errData);
+}
+
+
+void errorInvalidComparison(long lineNb, const char *out, error_t *errData){
+    char *errType = "Comparison Error";
+    char errDetails[64];
+    char errLocation[64];
+
+    sprintf(errDetails, "Invalid comparison");
+    sprintf(errLocation, "File %s, line %ld", errData->inputFile, lineNb);
+
+    displayError(errType, errDetails, errLocation, out, errData);
+}
+
+
+void errorTypeIncompatibility(char *type1, char *type2, long lineNb, const char *out, error_t *errData){
+    char *errType = "Type Error";
+    char errDetails[64];
+    char errLocation[64];
+
+    sprintf(errDetails, "%s cannot be compared with %s", type1, type2);
+    sprintf(errLocation, "File %s, line %ld", errData->inputFile, lineNb);
+
+    displayError(errType, errDetails, errLocation, out, errData);
+}
+
+
+void errorSizeIncompatibility(char *var1, char *var2, long lineNb, const char *out, error_t *errData){
+    char *errType = "Size Error";
+    char errDetails[64];
+    char errLocation[64];
+
+    sprintf(errDetails, "%s's size is incompatible with %s's size", var1, var2);
+    sprintf(errLocation, "File %s, line %ld", errData->inputFile, lineNb);
+
+    displayError(errType, errDetails, errLocation, out, errData);
+}
+
+
+void errorFilename(const char *filename, const char *out, error_t *errData){
+    char *errType = "Filename Error";
+    char errDetails[64];
+    char errLocation[64] = "";
+
+    sprintf(errDetails, "The filename '%s' should end by .aop and be at least 5 characters", filename);
+
+    displayError(errType, errDetails, errLocation, out, errData);
+}
+
+
+void errorLineCharactersExceed(long lineNb, const char *out, error_t *errData){
+    char *errType = "Line Characters Exceed Error";
+    char errDetails[64];
+    char errLocation[64];
+
+    sprintf(errDetails, "The lines of a file shouldn’t exceed 64 characters each");
+    sprintf(errLocation, "File %s, line %ld", errData->inputFile, lineNb);
+
+    displayError(errType, errDetails, errLocation, out, errData);
+}
+
+
+void errorNonExistent(const char *varName, long lineNb, const char *out, error_t *errData){
+    char *errType = "Non-existent Error";
+    char errDetails[64];
+    char errLocation[64];
+
+    sprintf(errDetails, "'%s' wasn't defined", varName);
+    sprintf(errLocation, "File %s, line %ld", errData->inputFile, lineNb);
+
+    displayError(errType, errDetails, errLocation, out, errData);
+}
+
+
+// ---------------------------------BUILDER ERROR-----------------------------------------
+
+void errorLifoSize(const char *out, error_t *errData){
+    char *errType = "Lifo Size Error";
+    char errDetails[64];
+
+    sprintf(errDetails, "Error: comparison lifo isn't empty");
+
+    displayError(errType, errDetails, NULL, out, errData);
+}
+
+
+void errorBuildComparison(const char *out, error_t *errData){
+    char *errType = "Build Comparison Error";
+    char errDetails[64];
+
+    sprintf(errDetails, "Error: unknown comparison statement");
+
+    displayError(errType, errDetails, NULL, out, errData);
+}
+
+
+void errorLabelDeclaration(const char *label, const char *out, error_t *errData){
+    char *errType = "Label Declaration Error";
+    char errDetails[64];
+
+    sprintf(errDetails, "Label %s already declared", label);
+
+    displayError(errType, errDetails, NULL, out, errData);
+}
+
+
+void errorEmptyLifo(const char *out, error_t *errData){
+    char *errType = "Empty Lifo Error";
+    char errDetails[64];
+
+    sprintf(errDetails, "Error: Lifo is empty");
+
+    displayError(errType, errDetails, NULL, out, errData);
+}
+
+//-------------------BIN CONVERTER ERROR-------------------------------------
+
+void errorFileDetection(const char *filename, const char *out, error_t *errData){
+    char *errType = "File Detection Error";
+    char errDetails[64];
+
+    sprintf(errDetails, "Error: no such file or directory '%s'", filename);
+
+    displayError(errType, errDetails, NULL, out, errData);
+}
+
+
+void errorFileType(const char *filename, const char *out, error_t *errData){
+    char *errType = "File Type Error";
+    char errDetails[64];
+
+    sprintf(errDetails, "Error: this file '%s' couldn’t be used for bin conversion", filename);
+
+    displayError(errType, errDetails, NULL, out, errData);
+}
+
+
+void errorFileIssues(const char *filename, const char *out, error_t *errData){
+    char *errType = "File Issues Error";
+    char errDetails[64];
+
+    sprintf(errDetails, "Error: the targeted file '%s' contains errors and couldn't be converted properly", filename);
+
+    displayError(errType, errDetails, NULL, out, errData);
+}
+
+
+void errorConversion(const char *variable, const char *issue, const char *out, error_t *errData){
+    char *errType = "Conversion Error";
+    char errDetails[64];
+
+    sprintf(errDetails, "The variable %s %s", variable, issue);
+
+    displayError(errType, errDetails, NULL, out, errData);
+}
+
+
+// ---------RUNNER ERRORS---------
+
+void errorMissingDependencies(const char *details, const char *out, error_t *errData){
+    char *errType = "Missing Dependencies Error";
+    char errDetails[64];
+
+    sprintf(errDetails, "These libraries/components are undefined: %s", details);
+
+    displayError(errType, errDetails, out, errData);
+}
+
+
+void errorResourceExhaustion(const char *out, error_t *errData){
+    char *errType = "Resource Exhaustion Error";
+    char errDetails[64] = "The system doesn't have any resources left.";
+
+    displayError(errType, errDetails, NULL, out, errData);
+}
+
+
+void errorPathIssues(const char *path, const char *out, error_t *errData){
+    char *errType = "Path Issues Error";
+    char errDetails[64];
+
+    sprintf(errDetails, "Incorrect file paths: %s", path);
+
+    displayError(errType, errDetails, NULL, out, errData);
+}
+
+
+void errorEnvironmentIncompatibility(const char *os, const char *out, error_t *errData){
+    char *errType = "Environment Incompatibility Error";
+    char errDetails[64];
+
+    sprintf(errDetails, "The current operating system '%s' is unable to run this program", os);
+
+    displayError(errType, errDetails, NULL, out, errData);
+}
+
+
+void errorTimeout(const char *program, const char *out, error_t *errData){
+    char *errType = "Timeout Error";
+    char errDetails[64];
+
+    sprintf(errDetails, "The program '%s' didn’t respond anymore", program);
+
+    displayError(errType, errDetails, NULL, out, errData);
 }
 
 
--- src/error.h
+++ src/error.h
@@ -42,7 +42,197 @@ void errorInstruction(char* inst, instNode_t *node, const char* out, error_t *er
 */
 void errorLineSize(long lineNb, const char* out, error_t *errData);
 
-// ---------- Bin converting errors ----------
+/*
+    Throw an error when no instruction is found in the input line
+    params:
+        lineNb: Line number
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorInstructionMissing(long lineNb, const char *out, error_t *errData);
+
+/*
+    Throw an error for an invalid register
+    params:
+        reg: Register
+        lineNb: Line number
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorInvalidRegister(char *reg, long lineNb, const char *out, error_t *errData);
+
+/*
+    Throw an error for an unexpected symbol
+    params:
+        symbol: Unexpected symbol
+        lineNb: Line number
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorUnexpectedSymbol(char symbol, long lineNb, const char *out, error_t *errData);
+
+/*
+    Throw an error when a symbol is missing between arguments
+    params:
+        symbol: Missing symbol
+        lineNb: Line number
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorMissingSymbol(char symbol, long lineNb, const char *out, error_t *errData);
+
+/*
+    Throw an error for an invalid comparison
+    params:
+        lineNb: Line number
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorInvalidComparison(long lineNb, const char *out, error_t *errData);
+
+/*
+    Throw an error for type incompatibility in comparisons
+    params:
+        type1: First type
+        type2: Second type
+        lineNb: Line number
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorTypeIncompatibility(char *type1, char *type2, long lineNb, const char *out, error_t *errData);
+
+/*
+    Throw an error for size incompatibility in comparisons
+    params:
+        var1: First variable
+        var2: Second variable
+        lineNb: Line number
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorSizeIncompatibility(char *var1, char *var2, long lineNb, const char *out, error_t *errData);
+
+// ---------- Builder Errors ----------
+
+/*
+    Throw an error when the comparison lifo isn't empty
+    params:
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorLifoSize(const char *out, error_t *errData);
+
+/*
+    Throw an error when an unknown comparison statement is encountered
+    params:
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorBuildComparison(const char *out, error_t *errData);
+
+/*
+    Throw an error when a label is already declared
+    params:
+        label: Label that is already declared
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorLabelDeclaration(const char *label, const char *out, error_t *errData);
+
+/*
+    Throw an error when the lifo is empty
+    params:
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorEmptyLifo(const char *out, error_t *errData);
+
+// ---------- Bin Converter Errors ----------
+
+/*
+    Throw an error when the specified file is not found
+    params:
+        filename: Name of the file that was not found
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorFileDetection(const char *filename, const char *out, error_t *errData);
+
+/*
+    Throw an error when the file type is not suitable for binary conversion
+    params:
+        filename: Name of the file with the wrong type
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorFileType(const char *filename, const char *out, error_t *errData);
+
+/*
+    Throw an error when the targeted file contains errors and can't be converted
+    params:
+        filename: Name of the file containing errors
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorFileIssues(const char *filename, const char *out, error_t *errData);
+
+/*
+    Throw a conversion error with a specific message
+    params:
+        variable: Name of the variable causing the error
+        issue: Description of the conversion issue
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorConversion(const char *variable, const char *issue, const char *out, error_t *errData);
+
+
+// ---------- Runner Errors ----------
+
+/*
+    Throw an error when there are missing dependencies
+    params:
+        details: Description of the missing dependencies
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorMissingDependencies(const char *details, const char *out, error_t *errData);
+
+/*
+    Throw an error when the system runs out of resources
+    params:
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorResourceExhaustion(const char *out, error_t *errData);
+
+/*
+    Throw an error for incorrect file paths
+    params:
+        path: Incorrect file path
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorPathIssues(const char *path, const char *out, error_t *errData);
+
+/*
+    Throw an error when the current environment is incompatible with the program
+    params:
+        os: Operating system or environment that is incompatible
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorEnvironmentIncompatibility(const char *os, const char *out, error_t *errData);
+
+/*
+    Throw a timeout error for a non-responsive program
+    params:
+        program: Name or description of the program that did not respond
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorTimeout(const char *program, const char *out, error_t *errData);
+
 
 // ---------- Fatal errors ----------
 
@@ -91,6 +281,37 @@ void errorTooManyArg(const char* out, error_t *errData);
         errData: Error history
 */
 void errorNoArg(const char* out, error_t *errData);
+
+// ---------- Semantic Errors ----------
+
+/*
+    Throw an error for incorrect filename format
+    params:
+        filename: File's name
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorFilename(const char *filename, const char *out, error_t *errData);
+
+/*
+    Throw an error when line characters exceed the maximum allowed
+    params:
+        lineNb: Line number
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorLineCharactersExceed(long lineNb, const char *out, error_t *errData);
+
+/*
+    Throw an error for using a non-existent variable or register
+    params:
+        varName: Variable/Register name
+        lineNb: Line number
+        out: Target file (can be NULL)
+        errData: Error history
+*/
+void errorNonExistent(const char *varName, long lineNb, const char *out, error_t *errData);
+
 
 // ---------- Display error messages ----------
 
